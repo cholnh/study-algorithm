3a55f7d68bd1c79dfbc56274c0019d051873cbb8
Nzzi <cholnh1@naver.com>
2021-07-10 21:28:05 +0900

    add cos

---
+++ src/com/nzzi/study/cos/CosEx2.java
@@ -0,0 +1,79 @@
+package com.nzzi.study.cos;
+
+public class CosEx2 {
+    public int solution(int n) {
+        // Write code here.
+        int answer = 0;
+
+        int start = 1;
+
+        while (n > 0) {
+            answer += start + calculate(n, start);
+            start = next(n, start);
+            n -= 2;
+        }
+
+        return answer;
+    }
+
+    private int calculate(int n, int x) {
+        if (n == 1)
+            return 0;
+        return 2*(n - 1) + x;
+    }
+
+    private int next(int n, int x) {
+        return 4*(n - 1) + x;
+    }
+
+    // The following is main method to output testcase.
+    public static void main(String[] args) {
+
+
+        CosEx2 sol = new CosEx2();
+        Solution sol2 = new Solution();
+
+        for (int i = 1; i < 1000; i++) {
+            System.out.println("sol1] " + i + " => " + sol.solution(i));
+            System.out.println("sol2] " + i + " => " + sol2.solution(i));
+            System.out.println(sol.solution(i) == sol2.solution(i));
+            System.out.println();
+        }
+
+    }
+}
+
+class Solution{
+    int[][] pane;
+    int dx[] = {0, 1, 0, -1};
+    int dy[] = {1, 0, -1, 0};
+    boolean inRange(int i, int j, int n){
+        return 0 <= i && i < n && 0 <= j && j < n;
+    }
+    public int solution(int n){
+        pane = new int[n][n];
+        int ci = 0;
+        int cj = 0;
+        int num = 1;
+        while(inRange(ci, cj, n) && pane[ci][cj] == 0){
+            for(int k = 0; k < 4; k++){
+                if(!inRange(ci, cj, n) || pane[ci][cj] != 0) break;
+                while(true){
+                    pane[ci][cj] = num++;
+                    int ni = ci + dy[k];
+                    int nj = cj + dx[k];
+                    if(!inRange(ni, nj, n) || pane[ni][nj] != 0){
+                        ci += dy[(k + 1) % 4];
+                        cj += dx[(k + 1) % 4];
+                        break;
+                    }
+                    ci = ni;
+                    cj = nj;
+                }
+            }
+        }
+        int ans = 0;
+        for(int i = 0; i < n; i++) ans += pane[i][i];
+        return ans;
+    }
+}
---
+++ src/com/nzzi/study/find/FindExample.java
@@ -0,0 +1,13 @@
+package com.nzzi.study.find;
+
+public class FindExample {
+    public static void find(String text, String pattern) {
+        int count = 0;
+        int index = 0;
+
+
+
+        System.out.println(count);
+        System.out.println(index);
+    }
+}
